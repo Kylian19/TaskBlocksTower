3bd2c9d4a66d0bd73697bc98cac4b8c3845b60d0
Kylian TRONCHON <kylian.tronchon@apside-groupe.com>
2024-01-18 11:32:36 +0100

    Add new routes and icons to sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { PlanningComponent } from './planning/planning.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent},
     { path: 'profile', component: ProfileComponent},
-    { path: 'settings', component: SettingsComponent}
+    { path: 'planning', component: PlanningComponent},
+    { path: 'charts', component: PlanningComponent},
+    { path: 'files', component: PlanningComponent},
+    { path: 'maps', component: PlanningComponent},
+    { path: 'settings', component: SettingsComponent},
+    { path: 'log-out', component: PlanningComponent}
 ];
 
 @NgModule({
